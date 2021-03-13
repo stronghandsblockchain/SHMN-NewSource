afeb238e4feab2b95306ad6a4e59edb44b7211fb
01Fedda <31264563+01Fedda@users.noreply.github.com>
2021-03-12 19:32:08 -0500

    Copyright update

--- src/init.cpp
+++ src/init.cpp
@@ -511,7 +511,7 @@ std::string LicenseInfo()
            "\n" +
            FormatParagraph(strprintf(_("Copyright (C) 2015-%i The Pivx core Developers"), COPYRIGHT_YEAR)) + "\n" +
            "\n" +
-           FormatParagraph(strprintf(_("Copyright (C) %i The Shmn core Developers"), COPYRIGHT_YEAR)) + "\n" +
+           FormatParagraph(strprintf(_("Copyright (C) 2018-%i The Shmn core Developers"), COPYRIGHT_YEAR2)) + "\n" +
            "\n" +
            FormatParagraph(_("This is experimental software.")) + "\n" +
            "\n" +
