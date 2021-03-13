0711766b64f6503325c4e839f76286755db62c39
01Fedda <31264563+01Fedda@users.noreply.github.com>
2021-03-12 19:37:01 -0500

    Copyright update

--- src/clientversion.h
+++ src/clientversion.h
@@ -27,6 +27,7 @@
  * Todo: update this when changing our copyright comments in the source
  */
 #define COPYRIGHT_YEAR 2018
+#define COPYRIGHT_YEAR2 2021
 
 #endif //HAVE_CONFIG_H
 
@@ -38,7 +39,7 @@
 #define DO_STRINGIZE(X) #X
 
 //! Copyright string used in Windows .rc files
-#define COPYRIGHT_STR "2009-" STRINGIZE(COPYRIGHT_YEAR) " The Bitcoin Core Developers, 2014-" STRINGIZE(COPYRIGHT_YEAR) " The Dash Core Developers, 2015-" STRINGIZE(COPYRIGHT_YEAR) " The Pivx Core Developers, 2017-" STRINGIZE(COPYRIGHT_YEAR) " The shmn core Developers, 2018"
+#define COPYRIGHT_STR "2009-" STRINGIZE(COPYRIGHT_YEAR) " The Bitcoin Core Developers, 2014-" STRINGIZE(COPYRIGHT_YEAR) " The Dash Core Developers, 2015-" STRINGIZE(COPYRIGHT_YEAR) " The Pivx Core Developers, 2017-" STRINGIZE(COPYRIGHT_YEAR) " The shmn core Developers, 2018-" STRINGIZE(COPYRIGHT_YEAR2)
 
 /**
  * shmnd-res.rc includes this file, but it cannot cope with real c++ code.
